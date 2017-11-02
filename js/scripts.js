74297030167201615cbc96c7b05aba5252f51ab3
Newton Bii <biinewton382@gmail.com>
2017-11-02 15:43:25 +0300

    Add javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $("form.input-form").submit(function(event){
+
 var date = $("input#date").val();
-$("#list p").append(date);
-  event.preventDefault();
+var activity =$("input#activity").val();
+var from = $("input#from").val();
+var to = $("input#to").val();
+
+
+$("#list ul").append("<li> From " + from+ " to "+to+", on "+date+" : Do "+activity+".</li>"+"<input type='radio'>Done</input>"+"<input type='radio>TO DO</input>'");
+
+event.preventDefault();
 });
